6bdc50e25cdd025c09a7deef9ae954a813d6f355
Werick Lima <87555656+WerickL@users.noreply.github.com>
2022-05-07 00:58:25 -0300

    responsive

--- NLW/assets/scripts/main.js
+++ NLW/assets/scripts/main.js
@@ -1,4 +1,10 @@
+window.addEventListener('scroll', onScroll)
+onScroll()
 function onScroll(){
+  showNav()
+  showToTop()    
+}
+function showNav(){
   if(scrollY > 0){
     navi.classList.add('scroll')
   }
@@ -11,10 +17,17 @@ function openMenu(){
 }
 function closeMenu(){
   document.body.classList.remove('menu-expanded')
-   
 }
 ScrollReveal({
   origin: 'left',
   duration: 700,
   distance: '50px',
 }).reveal(`#main,.cont-statistics,img, #services header, .card, #about header, #about .content, #contato header, #contato .content`);
+function showToTop(){
+  if(scrollY > 550){
+    toTop.classList.add('show')
+  }
+  else{
+    toTop.classList.remove('show')
+  }
+}
